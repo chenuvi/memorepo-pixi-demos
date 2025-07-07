68a964062169f4e953567b73c61a516a747c6c1a
kunchen16 <kunchen16@iflytek.com>
2025-07-07 17:15:18 +0800

    biild 11

--- apps/fish-pond/src/types.ts
+++ apps/fish-pond/src/types.ts
@@ -1,8 +1,8 @@
-import { Sprite as PixiSprite } from 'pixi.js';
+import 'pixi.js';
 
 // 扩展 Pixi.js 的 Sprite 类型
 declare module 'pixi.js' {
-  interface Sprite extends PixiSprite {
+  interface Sprite {
     direction: number;
     speed: number;
     turnSpeed: number;
